909f1e070ab1b3372bc2479a16b1ad69350f6eec
jamilxt <jamilxt@gmail.com>
2025-05-06 23:23:40 +0600

    Apply Swagger Tag for Project Rest APIs

--- src/main/java/com/pondit/portfolio/controller/rest/ProjectRestController.java
+++ src/main/java/com/pondit/portfolio/controller/rest/ProjectRestController.java
@@ -2,21 +2,25 @@ package com.pondit.portfolio.controller.rest;
 
 import com.pondit.portfolio.model.domain.Project;
 import com.pondit.portfolio.model.dto.CreateProjectRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Tag(name = "Project Resource", description = "API for managing projects")
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectRestController {
     List<Project> projects = new ArrayList<>();
 
+    @Tag(name = "Get All Projects", description = "Get all projects")
     @GetMapping
     public List<Project> getAllProjects() {
         return projects;
     }
 
+    @Tag(name = "Create Project", description = "Create a new project")
     @PostMapping
     public Project createProject(@RequestBody CreateProjectRequest request) {
         // TODO: validate input
