96807d3ad662d9f3c97c53e063fc115ae3d6fe27
jamilxt <jamilxt@gmail.com>
2025-05-21 22:47:39 +0600

    get site properties value using @Value

--- src/main/java/com/pondit/portfolio/controller/web/RootController.java
+++ src/main/java/com/pondit/portfolio/controller/web/RootController.java
@@ -2,6 +2,7 @@ package com.pondit.portfolio.controller.web;
 
 import com.pondit.portfolio.config.ResumeConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +12,16 @@ public class RootController {
     @Autowired
     ResumeConfig resumeConfig;
 
+    @Value("${site.title}")
+    String siteTitle;
+
+    @Value("${site.description}")
+    String siteDescription;
+
     @GetMapping
     public String indexPage(Model model) {
+        model.addAttribute("siteTitle", siteTitle);
+        model.addAttribute("siteDescription", siteDescription);
         model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
         model.addAttribute("education", resumeConfig.getEducation());
         model.addAttribute("experience", resumeConfig.getExperience());
