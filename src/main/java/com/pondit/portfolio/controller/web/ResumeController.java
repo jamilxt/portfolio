d55849646411a79d13510fd8116dd468e4f00104
jamilxt <jamilxt@gmail.com>
2025-06-13 23:23:25 +0600

    Implement blog and resume pages with new controllers and views
    Added `BlogController` and `ResumeController` to handle blog and resume functionality. Introduced dedicated templates for blog (`blog/index.html`, `blog/detail.html`) and resume (`resume/index.html`) pages. Simplified `RootController` to route directly to the blog index.

---
+++ src/main/java/com/pondit/portfolio/controller/web/ResumeController.java
@@ -0,0 +1,42 @@
+package com.pondit.portfolio.controller.web;
+
+import com.pondit.portfolio.config.ResumeConfig;
+import com.pondit.portfolio.service.ProjectService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping("/resume")
+@Controller
+public class ResumeController {
+    @Autowired
+    ResumeConfig resumeConfig;
+    @Autowired
+    ProjectService projectService;
+
+    @Value("${site.title}")
+    String siteTitle;
+
+    @Value("${site.description}")
+    String siteDescription;
+
+    @GetMapping
+    public String indexPage(Model model) {
+        log.debug("Setting attributes for index page");
+        model.addAttribute("siteTitle", siteTitle);
+        model.addAttribute("siteDescription", siteDescription);
+        model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
+        model.addAttribute("education", resumeConfig.getEducation());
+        model.addAttribute("experience", resumeConfig.getExperience());
+        model.addAttribute("skills", resumeConfig.getSkills());
+        model.addAttribute("projects", projectService.getAllProjects(Pageable.unpaged()));
+        log.debug("Rendering index page");
+        return "resume/index";
+    }
+}
--- src/main/java/com/pondit/portfolio/controller/web/RootController.java
+++ src/main/java/com/pondit/portfolio/controller/web/RootController.java
@@ -1,41 +1,13 @@
 package com.pondit.portfolio.controller.web;
 
-import com.pondit.portfolio.config.ResumeConfig;
-import lombok.extern.slf4j.Slf4j;
-import com.pondit.portfolio.service.ProjectService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class RootController {
-    @Autowired
-    ResumeConfig resumeConfig;
-    @Autowired
-    ProjectService projectService;
-
-    @Value("${site.title}")
-    String siteTitle;
-
-    @Value("${site.description}")
-    String siteDescription;
-
     @GetMapping
     public String indexPage(Model model) {
-        log.debug("Setting attributes for index page");
-        model.addAttribute("siteTitle", siteTitle);
-        model.addAttribute("siteDescription", siteDescription);
-        model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
-        model.addAttribute("education", resumeConfig.getEducation());
-        model.addAttribute("experience", resumeConfig.getExperience());
-        model.addAttribute("skills", resumeConfig.getSkills());
-        model.addAttribute("projects", projectService.getAllProjects(Pageable.unpaged()));
-        log.debug("Rendering index page");
-        return "index";
+        return "blog/index";
     }
-
 }
