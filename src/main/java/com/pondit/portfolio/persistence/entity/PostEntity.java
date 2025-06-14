8860e9a6358177574d5b4bdd6a6ee8fe5bfad6d5
jamilxt <jamilxt@gmail.com>
2025-06-14 23:44:44 +0600

    Add Post management with REST API, service, and persistence
    Implemented a Post entity, repository, and service layer. Added REST API for retrieving posts with pagination support. Domain and entity objects are mapped to streamline data handling.
    
    (cherry picked from commit b5ff77026e0b1126ceb11043ad5316d611caba42)

---
+++ src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
@@ -0,0 +1,32 @@
+package com.pondit.portfolio.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "post")
+public class PostEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Setter
+    private String title;
+
+    @Setter
+    private String content;
+
+    @Setter
+    private String slug;
+
+    @Setter
+    @Column(name = "is_published")
+    private Boolean published;
+
+    @Setter
+    private LocalDateTime publishedAt;
+}
