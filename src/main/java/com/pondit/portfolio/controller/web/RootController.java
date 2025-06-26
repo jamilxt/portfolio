2dff06829209eef8bb0c23be64c5dbb3485763af
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-06-27 01:50:00 +0600

    Fetch top two recent blog posts and display on home page

--- src/main/java/com/pondit/portfolio/controller/web/RootController.java
+++ src/main/java/com/pondit/portfolio/controller/web/RootController.java
@@ -1,13 +1,30 @@
 package com.pondit.portfolio.controller.web;
 
+import com.pondit.portfolio.persistence.entity.blogEntity.PostEn;
+import com.pondit.portfolio.persistence.repository.blogRepo.PostEnRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
-    @GetMapping
-    public String indexPage(Model model) {
-        return "blog/index";
+
+    private final PostEnRepository postRepository;
+
+    public RootController(PostEnRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        Pageable topTwo = PageRequest.of(0, 2, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Page<PostEn> recentPosts = postRepository.findAll(topTwo);
+
+        model.addAttribute("posts", recentPosts.getContent());
+        return "blog/index"; // This points to templates/index.html
     }
 }
