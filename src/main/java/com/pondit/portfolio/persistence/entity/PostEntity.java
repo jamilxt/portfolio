563688b044620015f9411ca833827aba206bc393
Asad_Ullah <au10050305@gmail.com>
2025-06-27 20:57:42 +0600

    feat: implement create operations for posts with pagination and creation timestamp

--- src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
+++ src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
@@ -3,6 +3,7 @@ package com.pondit.portfolio.persistence.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -29,5 +30,6 @@ public class PostEntity {
     private Boolean published;
 
     @Setter
+    @CreationTimestamp
     private LocalDateTime publishedAt;
 }
