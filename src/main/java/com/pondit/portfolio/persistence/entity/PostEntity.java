3d7187ae6b8e2c86af3d5d8965f84f54081d4031
jamilxt <jamilxt@gmail.com>
2025-06-27 23:40:09 +0600

    feat: add unique constraint on slug column in PostEntity

--- src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
+++ src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
@@ -3,13 +3,14 @@ package com.pondit.portfolio.persistence.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
-@Table(name = "post")
+@Table(name = "post", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "slug")
+})
 public class PostEntity {
     @Id
     @GeneratedValue
