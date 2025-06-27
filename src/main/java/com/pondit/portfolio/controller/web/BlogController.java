470dd2a767fad70ca4ebe6dbf9bd2e82073e34fe
jamilxt <jamilxt@gmail.com>
2025-06-27 22:35:30 +0600

    feat: enhance BlogController to display paginated posts on index page

--- src/main/java/com/pondit/portfolio/controller/web/BlogController.java
+++ src/main/java/com/pondit/portfolio/controller/web/BlogController.java
@@ -1,15 +1,29 @@
 package com.pondit.portfolio.controller.web;
 
+import com.pondit.portfolio.persistence.entity.PostEntity;
+import com.pondit.portfolio.persistence.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/blog")
+@RequestMapping({"/", "/blog"})
+@RequiredArgsConstructor
 @Controller
 public class BlogController {
+    private final PostRepository postRepository;
+
     @GetMapping
-    public String indexPage() {
+    public String indexPage(Model model) {
+        Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "publishedAt"));
+        Page<PostEntity> posts = postRepository.findAll(pageable);
+        model.addAttribute("controllerPosts", posts.getContent());
         return "blog/index";
     }
 
