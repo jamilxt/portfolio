4ffa87d45dec5783e45de0b816df418420afac96
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-06-28 22:21:48 +0600

    Make PostService dynamic

--- src/main/java/com/pondit/portfolio/controller/rest/PostRestController.java
+++ src/main/java/com/pondit/portfolio/controller/rest/PostRestController.java
@@ -1,26 +1,56 @@
 package com.pondit.portfolio.controller.rest;
 
+import com.pondit.portfolio.exception.custom.NotFoundException;
 import com.pondit.portfolio.model.domain.Post;
+import com.pondit.portfolio.model.dto.CreatePostRequest;
+import com.pondit.portfolio.model.dto.UpdatePostRequest;
 import com.pondit.portfolio.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Post Resource", description = "API for managing posts")
+@Tag(name = "Project Resource", description = "API for managing projects")
 @RestController
 @RequestMapping("api/posts")
+@AllArgsConstructor
 public class PostRestController {
-    @Autowired
-    PostService postService;
+    private final PostService postService;
 
     @Operation(summary = "Get all posts")
     @GetMapping
     public List<Post> getAllPosts(@ParameterObject Pageable pageable) {
         return postService.getAllPosts(pageable);
+    }
+
+    @Operation(summary = "Create a new post")
+    @PostMapping
+    public void createPost(@RequestBody CreatePostRequest request) {
+        // TODO: validate input
+        postService.create(request);
+    }
+
+    @Operation(summary = "Get a post by id")
+    @GetMapping("{id}")
+    public ResponseEntity<Post> getPost(@PathVariable Long id) throws NotFoundException {
+        Post post = postService.getById(id);
+        return ResponseEntity.ok(post);
+    }
+
+    @Operation(summary = "Update a post by id")
+    @PutMapping("{id}")
+    public void updatePost(@PathVariable Long id, @RequestBody UpdatePostRequest request) throws NotFoundException {
+        postService.update(id, request);
+    }
+
+    @Operation(summary = "Delete a post by id")
+    @DeleteMapping("{id}")
+    public void deletePost(@PathVariable Long id) throws NotFoundException {
+        postService.delete(id);
     }
 }
--- src/main/java/com/pondit/portfolio/controller/web/BlogController.java
+++ src/main/java/com/pondit/portfolio/controller/web/BlogController.java
@@ -1,15 +1,32 @@
 package com.pondit.portfolio.controller.web;
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.pondit.portfolio.service.PostService;
+import com.pondit.portfolio.model.domain.Post;
+
+import java.util.List;
+
 
 @RequestMapping("/blog")
 @Controller
+@AllArgsConstructor
 public class BlogController {
+
+    private final PostService postService;
+
     @GetMapping
-    public String indexPage() {
+    public String indexPage(Model model) {
+        Pageable pageable = PageRequest.of(0, 5, Sort.Direction.DESC, "publishedAt");
+        List<Post> posts = postService.getAllPosts(pageable);
+        model.addAttribute("postList", posts);
         return "blog/index";
     }
 
