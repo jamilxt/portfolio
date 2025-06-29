8f2c1f2cec37ad5a1ff50a6190fa231db49a6da5
ferdoushi <62441835+FerdoushiNahar@users.noreply.github.com>
2025-06-29 10:31:35 +0200

    Move The File

--- src/main/java/com/pondit/portfolio/model/domain/config/ResumeConfig.java
+++ src/main/java/com/pondit/portfolio/model/domain/config/ResumeConfig.java
@@ -1,4 +1,4 @@
-package com.pondit.portfolio.config;
+package com.pondit.portfolio.model.domain.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
