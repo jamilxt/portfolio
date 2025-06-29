469263c2d72109439bb9afba35fd34232d3c626e
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-06-29 12:24:55 +0600

    feat: add HTML preview for post content with line breaks.
    - Implemented getPreviewAsHtml() to convert first 5 lines of content into HTML with <br> tags

--- src/main/java/com/pondit/portfolio/model/domain/Post.java
+++ src/main/java/com/pondit/portfolio/model/domain/Post.java
@@ -1,13 +1,14 @@
 package com.pondit.portfolio.model.domain;
-
 import lombok.*;
-
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Post {
     private Long id;
     private String title;
@@ -15,4 +16,17 @@ public class Post {
     private String slug;
     private Boolean published;
     private LocalDateTime publishedAt;
+
+
+    public String getPreviewAsHtml() {
+        if (content == null) return "";
+
+        String[] lines = content.split("\\r?\\n");
+        String preview = Arrays.stream(lines)
+                .limit(5)
+                .collect(Collectors.joining("<br>"));
+
+        return preview + (lines.length > 5 ? "<br>..." : "");
+    }
+
 }
