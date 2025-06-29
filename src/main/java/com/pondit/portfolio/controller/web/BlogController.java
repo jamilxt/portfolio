fb38c0fcef9ee247f0cb772201efc36044768f88
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-06-29 12:22:10 +0600

    feat: Fetches post by ID implemented for blog detail page

--- src/main/java/com/pondit/portfolio/controller/web/BlogController.java
+++ src/main/java/com/pondit/portfolio/controller/web/BlogController.java
@@ -1,12 +1,8 @@
 package com.pondit.portfolio.controller.web;
-
 import com.pondit.portfolio.exception.custom.NotFoundException;
 import com.pondit.portfolio.model.domain.Post;
-import com.pondit.portfolio.persistence.entity.PostEntity;
-import com.pondit.portfolio.persistence.repository.PostRepository;
 import com.pondit.portfolio.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,9 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping({"/", "/blog"})
 @RequiredArgsConstructor
@@ -36,8 +30,6 @@ public class BlogController {
     @GetMapping("/blog/detail/{id}")
     public String getPostDetails(@PathVariable Long id, Model model) throws NotFoundException {
         Post post = postService.getById(id);
-        System.out.println("DEBUG: Retrieved post = " + post);
-
         if (post == null) {
             return "redirect:/error/404";
         }
