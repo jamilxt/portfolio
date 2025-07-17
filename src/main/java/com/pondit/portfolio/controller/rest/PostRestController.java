5fc2c3bec150d6a7710e0e44a760bbb7ce3bb7e3
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-07-18 01:00:51 +0600

    ref: rest api validation for create post and update post.

--- src/main/java/com/pondit/portfolio/controller/rest/PostRestController.java
+++ src/main/java/com/pondit/portfolio/controller/rest/PostRestController.java
@@ -4,10 +4,10 @@ import com.pondit.portfolio.exception.custom.NotFoundException;
 import com.pondit.portfolio.model.domain.Post;
 import com.pondit.portfolio.model.dto.CreatePostRequest;
 import com.pondit.portfolio.model.dto.UpdatePostRequest;
-import com.pondit.portfolio.model.dto.UpdateProjectRequest;
 import com.pondit.portfolio.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
@@ -30,13 +30,13 @@ public class PostRestController {
 
     @Operation(summary = "Create a new post")
     @PostMapping
-    public void createPost(CreatePostRequest createPostRequest) {
+    public void createPost(@Valid @RequestBody CreatePostRequest createPostRequest) {
        postService.create(createPostRequest);
     }
 
     @Operation(summary = "Update a post by id")
     @PutMapping("{id}")
-    public void updateProject(@PathVariable Long id, @RequestBody UpdatePostRequest request) throws NotFoundException {
+    public void updateProject(@PathVariable Long id,@Valid @RequestBody UpdatePostRequest request) throws NotFoundException {
         postService.update(id, request);
     }
 
--- src/main/java/com/pondit/portfolio/model/dto/CreatePostRequest.java
+++ src/main/java/com/pondit/portfolio/model/dto/CreatePostRequest.java
@@ -1,7 +1,19 @@
 package com.pondit.portfolio.model.dto;
 
-public record CreatePostRequest(String title,String content,
-                                 boolean published) {
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-}
+public record CreatePostRequest(
+        @NotBlank(message = "Title is mandatory")
+        @Size(min = 5, message = "Title must be at least 5 characters long")
+        String title,
+
+        @NotBlank(message = "Content is mandatory")
+        @Size(min = 10, message = "Content must be at least 10 characters long")
+        String content,
+
+        @NotNull(message = "Published status is required")
+        Boolean published
+) {}
