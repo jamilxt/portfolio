a19d756864e16f4bc82be58e57b6cde158d2b402
jamilxt <jamilxt@gmail.com>
2025-07-26 23:19:34 +0600

    fix: config Web Security Filter chain for Admin Panel

--- src/main/java/com/pondit/portfolio/config/SecurityConfig.java
+++ src/main/java/com/pondit/portfolio/config/SecurityConfig.java
@@ -2,20 +2,21 @@ package com.pondit.portfolio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
         http
+                .securityMatcher("/admin/**")
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/admin/**").authenticated()
                         .anyRequest().permitAll()
                 )
-                .formLogin(Customizer.withDefaults());
+                .formLogin(formLogin ->
+                        formLogin.loginPage("/admin/login"));
         return http.build();
     }
 }
