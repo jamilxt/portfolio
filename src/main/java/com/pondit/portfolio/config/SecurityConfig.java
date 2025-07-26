efeb99533ffd5a6417b29479a565464b4d6bd200
jamilxt <jamilxt@gmail.com>
2025-07-26 23:28:36 +0600

    config Rest API Security Filter Chain

--- src/main/java/com/pondit/portfolio/config/SecurityConfig.java
+++ src/main/java/com/pondit/portfolio/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pondit.portfolio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,6 +18,18 @@ public class SecurityConfig {
                 )
                 .formLogin(formLogin ->
                         formLogin.loginPage("/admin/login"));
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain restApiSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/**").authenticated()
+                )
+                // TODO: apply JWT Token - Use Custom Filter
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
