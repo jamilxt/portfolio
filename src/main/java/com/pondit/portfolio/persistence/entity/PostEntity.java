4cb0752120d1a6f21dcf8fa91244f40da20c8f48
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-08-07 00:44:16 +0600

    refine ID generation: specify GenerationType.IDENTITY in PostEntity

--- src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
+++ src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 })
 public class PostEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter
     private Long id;
 
