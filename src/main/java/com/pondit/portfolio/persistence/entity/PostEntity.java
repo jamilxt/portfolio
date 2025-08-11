23628d86e7def028e4b0228c19a1fe002a0159a0
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-08-12 00:02:40 +0600

    remove GenerationType.IDENTITY and revert to default @GeneratedValue

--- src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
+++ src/main/java/com/pondit/portfolio/persistence/entity/PostEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 })
 public class PostEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     @Setter
     private Long id;
 
